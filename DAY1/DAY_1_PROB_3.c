93cded208bb6021b65ed9117cb264530575ed06d
suramsaikumar <108974008+suramsaikumar@users.noreply.github.com>
2022-07-12 20:08:24 +0530

    Create DAY_1_PROB_3.c

---
+++ DAY1/DAY_1_PROB_3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int rad;
+    int v;
+    int m;
+    printf("Enter the radius:\n");
+    fflush(stdout);
+    scanf("%d",&rad);
+    v=3.1412*rad*rad;
+    printf("Total area of the field is %d\n",&m);
+}
