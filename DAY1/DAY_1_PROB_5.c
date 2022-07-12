e3ddac0aaac70acaeacce9e1c100dab6084b069b
suramsaikumar <108974008+suramsaikumar@users.noreply.github.com>
2022-07-12 20:12:47 +0530

    Create DAY_1_PROB_5.c

---
+++ DAY1/DAY_1_PROB_5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num1;int num2;
+    printf("Enter the number:\n");
+    fflush(stdout);
+    scanf("%d",&num1);
+    if(num2=num1%2==0)
+    {
+        printf("Entered number is even\n");
+    }
+    else
+    {
+        printf("Entered number is odd\n");
+    }
+}
