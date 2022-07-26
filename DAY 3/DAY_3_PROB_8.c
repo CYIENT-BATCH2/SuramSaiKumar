9ab0f87991b605a1b55500f9df50cb6f1b8048b3
suramsaikumar <108974008+suramsaikumar@users.noreply.github.com>
2022-07-26 15:40:32 +0530

    Create DAY_3_PROB_8.c

---
+++ DAY 3/DAY_3_PROB_8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("enter the charcter\n");
+	fflush(stdout);
+	scanf("%c",&ch);
+((ch>='a'||ch>='A')&&(ch<='z'||ch<='Z'))?printf("alphabet\n"):printf("not alphabet\n");
+
+}
+Footer
+© 2022 GitHu
