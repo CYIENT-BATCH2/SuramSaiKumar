61ec2e3ec3e5d92b1b5d44068ef1cc044721610d
suramsaikumar <108974008+suramsaikumar@users.noreply.github.com>
2022-07-26 15:43:27 +0530

    Create DAY_3_PROB_9(c).c

---
+++ DAY 3/DAY_3_PROB_9(c).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	long int num,pos=7;
+	printf("enter the number\n");
+	fflush(stdout);
+	scanf("%x",&num);
+	printf("%d\n",num);
+	loop:
+	if(pos>=0)
+	{
+		printf("%ld",(num>>pos)&1);
+		pos--;
+		goto loop;
+	}
+	}
